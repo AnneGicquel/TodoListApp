cbf658fbf27a94b22b43b883570a4d42800c639a
AnneGicquel <annelaedere@gmail.com>
2023-03-16 13:36:30 +0100

    Création des pages et du routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoryComponent } from './pages/history/history.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
+import { NoTaskComponent } from './pages/no-task/no-task.component';
+import { TaskListComponent } from './pages/task-list/task-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: NoTaskComponent
+  },
+  {
+    path: "new",
+    component: NewTaskComponent
+  },
+  {
+    path: "list",
+    component: TaskListComponent
+  },
+  {
+    path: "history",
+    component: HistoryComponent
+  }
+];
 
+// SCROLL POSITION au retour sur les pages!
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoTaskComponent } from './pages/no-task/no-task.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
+import { TaskListComponent } from './pages/task-list/task-list.component';
+import { HistoryComponent } from './pages/history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoTaskComponent,
+    NewTaskComponent,
+    TaskListComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
